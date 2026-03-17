86a61e03b58805e657e5581560b1d387f63f721b
Joe-metry <joemetry150@gmail.com>
2026-03-17 10:34:20 -0400

    Add initial implementation of simple counter app with styling and functionality

---
+++ counter/script/index.js
@@ -0,0 +1,12 @@
+
+let countUp = document.getElementById("count-up");
+let count = 0;
+
+function increment() {
+    count = count + 1;
+    countUp.innerText = count;
+}
+
+function save() {
+    
+}
