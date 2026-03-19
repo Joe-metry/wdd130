1a7c25f1a9e27efc8413dc450c2462d923600e28
Joe-metry <joemetry150@gmail.com>
2026-03-19 10:46:12 +0100

    Add display prompt for count history and adjust styles

--- counter/script/index.js
+++ counter/script/index.js
@@ -1,5 +1,7 @@
 
 let countUp = document.getElementById("count-up");
+let displayWidget = document.getElementById("display-widget");
+ 
 let count = 0;
 
 function increment() {
@@ -8,5 +10,10 @@ function increment() {
 }
 
 function save() {
-    
+    let delimiter = " - ";
+    displayWidget.innerText += " " + count + delimiter;
+    count = 0;
 }
+
+
+//1 Appreciation 2 Success in our academics 3 Financial breakthrough 4 Help get ride of bottlenecks 5 Help in my scholarship
